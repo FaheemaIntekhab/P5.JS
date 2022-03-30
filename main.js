db87fc9508c28516aaed68f199fbe98381a8feaa
FaheemaIntekhab <89588634+FaheemaIntekhab@users.noreply.github.com>
2022-03-30 09:11:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+createCanvas(400, 400);
+Circle(30, 30, 20);
+ellipse(56, 46, 55, 55);
+rect(30, 20, 55, 55);
+rect(30, 20, 55, 55, 20);
+rect(30, 20, 55, 55, 20, 15, 10, 5);
+video = createCapture(VIDEO);
